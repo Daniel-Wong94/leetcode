5aa4e4c8e7c55dee95e7f2845079c32d8b9f6495
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-06 20:46:05 -0500

    Time: 100 ms (58.16%), Space: 47.4 MB (8.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -5,12 +5,6 @@
 var moveZeroes = function(nums) {
     // count the number of zeros
     // for each 0, splice() and push() to the back of the array
-    let countOfZeroes = 0;
-    
-    for (const num of nums) {
-        if (num === 0) countOfZeroes++
-    }
-    
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === 0) {
            const zero = nums.splice(i, 1);
