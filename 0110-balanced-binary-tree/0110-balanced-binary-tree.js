9efe0732ee6033102efa0f03252493821cf61ca9
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-08 10:54:19 -0500

    Time: 78 ms (68.52%), Space: 47 MB (63.95%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -20,13 +20,17 @@ var isBalanced = function(root) {
 
 // return the height of left / right branches if still balanced, otherwise, bubble up a return value of -1
 var getHeight = function(root) {
+    // Base case for leaf nodes
     if (!root) return 0;
     
+    // recursive dfs both branches
     const leftHeight = getHeight(root.left)
     const rightHeight = getHeight(root.right)
     
+    // checks for unbalance and bubbles up the -1
     if (leftHeight === -1 || rightHeight === -1) return -1;
     if (Math.abs(leftHeight - rightHeight) > 1) return -1;
     
+    // if it hits this line, subtree is still balanced
     return 1 + Math.max(leftHeight, rightHeight)
 };
