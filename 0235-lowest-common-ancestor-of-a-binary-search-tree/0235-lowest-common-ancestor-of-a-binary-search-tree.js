b0cb10e1890ba252579cf5a361a705e26393b3c3
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-09 14:59:50 -0500

    Time: 247 ms (5.21%), Space: 59.4 MB (5.37%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+
+const lowestCommonAncestor = (root, p, q) => {
+    // get the path to p and q and then check which element appears in both paths first
+    const pathP = getPath(root, p.val)
+    const pathQ = getPath(root, q.val)
+    const setQ = new Set(pathQ.map(node => node.val))
+    
+    console.log(pathP, pathQ)
+    
+    for (const node of pathP) {
+        if (setQ.has(node.val)) {
+            return node;
+        } 
+    }
+};
+
+const getPath = (root, target) => {
+    if (!root) return null;
+    if (root.val === target) return [root];
+    
+    const left = getPath(root.left, target)
+    if (left) {
+        left.push(root)
+        return left;
+    }
+    
+    const right = getPath(root.right, target)
+    if (right) {
+        right.push(root)
+        return right;
+    }
+}  
