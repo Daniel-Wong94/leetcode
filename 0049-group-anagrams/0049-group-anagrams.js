03a47260cd834dfb68bbe9630be8e6916c9f8334
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-16 19:33:02 -0500

    Time: 140 ms (45.50%), Space: 53.2 MB (48.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,17 +3,26 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    // iterate through each word and sort its chars
-    // use the new word as a key in an object tracker
-    // the values will be an array of each word
     const tracker = {}
     
     for (const str of strs) {
-        const word = str.split("").sort().join("")
+        const key = getKey(str)
         
-        if (word in tracker) tracker[word].push(str)
-        else tracker[word] = [str]
+        if (!tracker[key]) tracker[key] = [str]
+        else tracker[key].push(str)
     }
     
     return Object.values(tracker)
-};
+}; 
+
+// n = max length (str1, str2); Time: O(n); Space: O(n)
+const getKey = str => {
+    const count = Array(26).fill(0)
+    
+    for (let i = 0; i < str.length; i++) {
+        const index = str.toLowerCase().charCodeAt(i) - 97
+        count[index]++
+    }
+    
+    return JSON.stringify(count);
+}
