8b7e3348a47e503b581f8aa552154d8c9ef43b75
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-19 17:10:44 -0500

    Time: 64 ms (74.70%), Space: 43.1 MB (33.46%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const result = []
+
+    const getCombos = (str="", open=0, close=0) => {
+        // pruning condition
+        if (open > n || close > n || close > open) return;
+
+        // base case
+        if (open === close && str.length === n * 2) {
+            result.push(str)
+            return
+        }
+
+        const addOpen = getCombos(str + "(", open + 1, close)
+        const addClose = getCombos(str + ")", open, close + 1)
+    }
+    
+    getCombos()
+    
+    return result;
+};
