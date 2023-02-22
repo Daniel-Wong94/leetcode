7db61ddd4276ccda93799f4402e5ab323b5508aa
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-22 14:55:11 -0500

    Time: 83 ms (63.90%), Space: 47 MB (16.94%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    if (!head) return head
+    let dummyHead = new ListNode()
+    let current = head
+    let previous = dummyHead
+    
+    dummyHead.next = current
+    
+    while (current) {
+        if (current.val === val) {
+            current = current.next
+            previous.next = current
+            continue
+        }    
+        
+        previous = current
+        current = current.next
+    }
+    
+    return dummyHead.next
+};
