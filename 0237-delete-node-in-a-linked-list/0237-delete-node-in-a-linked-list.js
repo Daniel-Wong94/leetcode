67487165350e18ccc845844aec843d4e41105694
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-22 16:04:52 -0500

    Time: 58 ms (98.89%), Space: 44.7 MB (6.99%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.js
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.js
@@ -12,7 +12,7 @@
 var deleteNode = function(node) {
     // if node is not null and the next node exists
     // set current node val to next val and point to the node after the next node
-    // else node must be null and 
+    // else node must be null and therefore nothing to remove
     if (node && node.next) {
         node.val = node.next.val
         node.next = node.next.next
