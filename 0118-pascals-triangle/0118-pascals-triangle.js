5d72498b0047e23f8725caeb3ca4fe5f8e36d881
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-22 12:58:05 -0500

    Time: 64 ms (69.70%), Space: 42.5 MB (33.53%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const result = []
+    
+    for (let i = 0; i < numRows; i++) {
+        const current = []
+        
+        for (let j = 0; j <= i; j++) {
+            if (j === 0 || j === i) current.push(1)
+            else {
+                current[j] = result[i - 1][j - 1] + result[i - 1][j]
+            }
+        }
+        
+        result.push(current)
+    }
+    
+    return result;
+};
