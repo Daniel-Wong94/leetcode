199c2570d5ed27d6c7205fe3388fc0e98b548c8b
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-22 12:59:50 -0500

    Time: 70 ms (40.17%), Space: 42.4 MB (33.53%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -6,10 +6,10 @@ var generate = function(numRows) {
     const result = []
     
     for (let i = 0; i < numRows; i++) {
-        const current = []
+        const current = Array(i + 1)
         
         for (let j = 0; j <= i; j++) {
-            if (j === 0 || j === i) current.push(1)
+            if (j === 0 || j === i) current[j] = 1
             else {
                 current[j] = result[i - 1][j - 1] + result[i - 1][j]
             }
