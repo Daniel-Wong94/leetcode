dc166db851e9cfd9c822c3357e38de2c1b5a42a1
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-23 19:08:28 -0500

    Time: 113 ms (31.62%), Space: 50.5 MB (11.71%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
@@ -12,13 +12,17 @@
  * @return {TreeNode}
  */
 var mergeTrees = function(root1, root2) {
+    // if one of the children do not exist, then the parent will point to one of them
     if (!root1) return root2
     if (!root2) return root1
     
+    // if both exists, just add root2.val to root1.val
     root1.val += root2.val
     
+    // recurse left and right, assuming it will return a node with updated value
     root1.left = mergeTrees(root1.left, root2.left)
     root1.right = mergeTrees(root1.right, root2.right)
     
+    // if both nodes exists, this will return the updated root1 node
     return root1
 };
