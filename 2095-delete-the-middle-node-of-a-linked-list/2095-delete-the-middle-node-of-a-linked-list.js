d80ba5070a3d7ebdc358e262b8868d5828f8e554
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-27 18:59:01 -0500

    Time: 844 ms (31.72%), Space: 82.2 MB (98.86%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -10,18 +10,33 @@
  * @return {ListNode}
  */
 var deleteMiddle = function(head) {
-    let slow = fast = head
-    let prev = new ListNode()
+//     let slow = fast = head
+//     let prev = new ListNode()
     
-    while (fast && fast.next) {
-        prev.next = slow
-        slow = slow.next
-        fast = fast.next.next
-        prev = prev.next
+//     while (fast && fast.next) {
+//         prev.next = slow
+//         slow = slow.next
+//         fast = fast.next.next
+//         prev = prev.next
+//     }
+    
+//     if (!prev.next) return null
+//     prev.next = slow.next
+    
+//     return head
+    
+    const nodeList = []
+    let curr = head
+    
+    while (curr) {
+        nodeList.push(curr)
+        curr = curr.next
     }
     
-    if (!prev.next) return null
-    prev.next = slow.next
+    let mid = Math.floor(nodeList.length / 2)
+    if (mid === 0) return null;
     
-    return head
+    nodeList[mid - 1].next = nodeList[mid].next
+    
+    return head;
 };
