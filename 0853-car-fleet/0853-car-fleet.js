c141ef75643b44233cc1e17c532e8e6605395150
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-27 13:51:36 -0500

    Time: 356 ms (38.75%), Space: 73.9 MB (32.50%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.js
+++ 0853-car-fleet/0853-car-fleet.js
@@ -5,14 +5,17 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
+    // pair position-speed and sort by position (descending)
     const pairs = position.map((car, i) => [car, speed[i]])
     pairs.sort((a, b) => b[0] - a[0])
     
+    // calculate time at finish line for each car
     const times = pairs.map(([p, s]) => ((target - p) / s))
     
     let count = 0
     let currFleet = -Infinity
     
+    // when a time is slower (greater than) the currFleet, that means a new fleet is being formed
     for (const time of times) {
         if (time > currFleet) {
             count++
