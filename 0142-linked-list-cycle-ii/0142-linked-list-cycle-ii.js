987dd52d9e305cb7b41ef3d873d3bf05129462a4
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-28 12:29:09 -0500

    Time: 74 ms (88.74%), Space: 45.5 MB (9.01%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    const nodes = new Set()
+    let curr = head
+    
+    while (curr) {
+        if (nodes.has(curr)) return curr
+        
+        nodes.add(curr)
+    
+        curr = curr.next
+    }
+        
+    return null
+};
