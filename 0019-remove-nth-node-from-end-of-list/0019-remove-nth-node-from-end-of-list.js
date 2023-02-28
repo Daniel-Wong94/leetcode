274ca2321d79904bffccf4212c6df288a835666d
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-28 14:19:50 -0500

    Time: 69 ms (53.41%), Space: 44.1 MB (5.21%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    const nodes = []
+    
+    let curr = head
+    while (curr) {
+        nodes.push(curr)
+        curr = curr.next
+    }
+    
+    let removeAt = nodes.length - n
+    let prev = head
+    curr = head
+    
+    if (removeAt === 0) return head.next
+    
+    while (removeAt) {
+        prev = curr
+        curr = curr.next
+
+        removeAt--
+    }
+    
+    if (!prev.next) return null
+    prev.next = curr.next
+    
+    return head;
+};
