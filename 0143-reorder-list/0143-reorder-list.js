653d9f8add1662f1e14c3c689875845968ae34a3
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-28 13:34:51 -0500

    Time: 89 ms (88.43%), Space: 49.5 MB (88.20%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    const nodes = []
+    let curr = head
+    
+    while (curr) {
+        nodes.push(curr)
+        curr = curr.next
+    }
+    
+    let left = 0
+    let right = nodes.length - 1
+    
+    while (left < right) {
+        const dist = Math.abs(left - right)
+        if (dist > 2) {
+            nodes[right].next = nodes[left].next
+            nodes[left].next = nodes[right]
+        } else if (dist === 2) {
+            nodes[right].next = nodes[left].next
+            nodes[left].next = nodes[right]
+            nodes[right].next.next = null
+        } else {
+            nodes[right].next = null
+            nodes[left].next = nodes[right]
+        }
+        
+        left++
+        right--
+    }
+    
+    return head
+};
