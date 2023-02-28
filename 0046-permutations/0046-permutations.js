97ce7cbf23f0a9980f7ba3d390a68f847da1804d
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-28 14:10:45 -0500

    Time: 63 ms (98.82%), Space: 44.9 MB (78.85%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -3,15 +3,20 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
+    // base case: empty array returns no permutations
     if (!nums.length) return [[]]
     
+    // hold the first element
     const firstEl = nums[0]
     
+    // recursive call on the rest of the array
     const permutations = permute(nums.slice(1))
     
     const result = []
     
+    // after recursive stack, permutations will hold different permutations without the first element
     for (const permutation of permutations) {
+        // put the first element back to each sub permutation in each position
         for (let i = 0; i <= permutation.length; i++) {
             result.push([...permutation.slice(0, i), firstEl, ...permutation.slice(i)])
         }
