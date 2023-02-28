1708f99e2b7bbd5bb0be5a86c5e5dfbe49b4a996
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-02-28 13:22:28 -0500

    Time: 73 ms (90.78%), Space: 45.5 MB (10.99%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -10,17 +10,37 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var detectCycle = function(head) {
-    const nodes = new Set()
-    let curr = head
+// var detectCycle = function(head) {
+//     const nodes = new Set()
+//     let curr = head
     
-    while (curr) {
-        if (nodes.has(curr)) return curr
+//     while (curr) {
+//         if (nodes.has(curr)) return curr
         
-        nodes.add(curr)
+//         nodes.add(curr)
     
-        curr = curr.next
+//         curr = curr.next
+//     }
+        
+//     return null
+// };
+
+var detectCycle = head => {
+    let slow = fast = head
+    
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+        
+        if (slow === fast) break;
     }
-        
-    return null
-};
+    
+    if (!fast || !fast.next) return null
+    
+    while (slow !== head) {
+        slow = slow.next
+        head = head.next
+    }
+    
+    return head
+}
