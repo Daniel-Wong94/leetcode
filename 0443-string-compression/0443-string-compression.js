7bdbf3b0227263be016b123cd0a4ff12834d81b6
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-03-02 14:55:05 -0500

    Time: 76 ms (56.06%), Space: 44.1 MB (66.78%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -3,18 +3,22 @@
  * @return {number}
  */
 var compress = function(chars) {
+    // set pointers
     let left = right = 0
     let current = chars[0]
     let count = 0
     
+    // iterate right pointer
     while (right < chars.length) {
+        // if match, increment count and pointer
+        // otherwise, mutate chars with current and digits, reset count and current char
         if (current === chars[right]) {
             count++
             right++
         } else {
             chars[left++] = current
             if (count > 1) {
-                const digits = String(count).split("")
+                const digits = String(count)
                 for (const digit of digits) {
                     chars[left++] = digit
                 }
@@ -24,11 +28,12 @@ var compress = function(chars) {
             current = chars[right]
         } 
     }
-            
+         
     chars[left++] = current
     
+    // check if we have any digits left to inject into chars   
     if (count > 1) {
-        const digits = String(count).split("")  
+        const digits = String(count)
         for (const digit of digits) {
             chars[left++] = digit
         }
