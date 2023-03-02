64a2142af7ac175539b201e25206352fbd4534cf
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-03-02 12:20:17 -0500

    Time: 137 ms (98.15%), Space: 64.9 MB (78.23%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -10,38 +10,34 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var goodNodes = function(root, max = -Infinity) {
-    // base case: leaf node
-    if (!root) return 0;
+// var goodNodes = function(root, max = -Infinity) {
+//     // base case: leaf node
+//     if (!root) return 0;
     
-    // recursive step left and right
-    const left = goodNodes(root.left, Math.max(max, root.val))
-    const right = goodNodes(root.right, Math.max(max, root.val))
+//     // recursive step left and right
+//     const left = goodNodes(root.left, Math.max(max, root.val))
+//     const right = goodNodes(root.right, Math.max(max, root.val))
     
-    // return value of 1 if node is "good"
-    const isGoodNode = root.val >= max
+//     // return value of 1 if node is "good"
+//     const isGoodNode = root.val >= max
     
-    return isGoodNode ? 1 + left + right : left + right
-};
+//     return isGoodNode ? 1 + left + right : left + right
+// };
 
-// var goodNodes = function (root) {
-//     const stack = [ root ]
-//     let count = 0
+var goodNodes = function (root) {
+    const stack = [ { node: root, max : root.val } ]
+    let count = 0
     
-//     while (stack.length) {
-//         const curr = stack.pop()
+    while (stack.length) {
+        let { node, max } = stack.pop()
         
-//         if (curr.left && curr.val <= curr.left.val) {
-//             count++
-//         }
+        if (node.val >= max) count++;
         
-//         if (curr.right && curr.val <= curr.right.val) {
-//             count++
-//         }
+        max = Math.max(max, node.val)
         
-//         if (curr.left) stack.push(curr.left)
-//         if (curr.right) stack.push(curr.right)
-//     }
+        if (node.left) stack.push({ node: node.left, max})
+        if (node.right) stack.push({ node: node.right, max})
+    }
     
-//     return count;
-// }
+    return count;
+}
