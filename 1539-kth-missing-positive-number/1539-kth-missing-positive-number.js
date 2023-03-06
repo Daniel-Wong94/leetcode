6024002cb7c3dfd3ba1201f29c9235b879bf8f28
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-03-06 11:47:54 -0500

    Time: 54 ms (94.85%), Space: 42.4 MB (51.03%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -4,6 +4,11 @@
  * @return {number}
  */
 var findKthPositive = function(arr, k) {
+    /*
+        maintain k pointed to the kth missing number (starts at k)
+        if arr[i] <= k, that means we know one number before k is checked off, so increment k to maintain its kth position
+        once arr[i] > k, that means k has landed on the kth missing number
+    */
     let i = 0
     
     while (arr[i] <= k + i) {
