23ee31bead6ea6479ebf42edec30b3b93630073c
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-03-18 12:16:07 -0400

    Time: 334 ms (73.07%), Space: 31.5 MB (15.51%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,13 +1,16 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        # nums_set for O(1) lookup
         nums_set = set(nums)
         subsets = {}
         max_subset = 0
         
+        # find start of each subset, initialize subset size to 1
         for num in nums:
             if (num - 1) not in nums_set:
                 subsets[num] = 1
         
+        # for each start, increment its value for each exisitng consecutive
         for subset in subsets:
             consecutive = subset + 1
             
