2a16af00bb8ad0bc1eabc69b8bfaab93a962d2e4
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-03-18 11:37:59 -0400

    Time: 82 ms (16.11%), Space: 13.9 MB (8.80%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,10 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # it will always be false if the number is negative
         if x < 0: return False
         
         num = x
         reversed_num = 0
         
+        # keep taking the last digit of num and putting in the front of reversed_num
         while x > 0:
             last_digit = x % 10
             reversed_num = reversed_num * 10 + last_digit
