cbf5471c8af55d969c697d327bf955d4ad66da2d
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-03 11:28:25 -0400

    Time: 192 ms (36.53%), Space: 54.5 MB (19.63%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ */
+var OrderedStream = function(n) {
+    this.stream = new Array(n)
+    this.pointer = 0
+};
+
+/** 
+ * @param {number} idKey 
+ * @param {string} value
+ * @return {string[]}
+ */
+OrderedStream.prototype.insert = function(idKey, value) {
+    this.stream[idKey - 1] = value
+    
+    const chunk = []
+
+    while (this.stream[this.pointer]) {
+        chunk.push(this.stream[this.pointer])
+        this.pointer++
+    }
+    
+    return chunk;
+};
+
+/** 
+ * Your OrderedStream object will be instantiated and called as such:
+ * var obj = new OrderedStream(n)
+ * var param_1 = obj.insert(idKey,value)
+ */
