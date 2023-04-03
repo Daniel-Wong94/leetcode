3b677c4998c563aaf19156f8e60bc147b714d1e6
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-03 15:58:35 -0400

    Time: 78 ms (60.85%), Space: 44.6 MB (65.31%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
+    // create one timeline of all start and end points
     const starts = intervals.map(i => i[0]).sort((a, b) => a - b)
     const ends = intervals.map(i => i[1]).sort((a, b) => a - b)
-    const n = intervals.length
-
+    
+    // whenever a start time exceeds an end time, that means the 
+    // previous end time opens up for a start time to use the same room
+    // therefore, you do not need to increment rooms when pointerEnd++
+    
     let pointerEnd = 0
     let rooms = 0
     
     for (const start of starts) {
-        if (start < ends[pointerEnd]) {
-            rooms++
-        } else {
-            pointerEnd++
-        }
+        start < ends[pointerEnd] ? rooms++ : pointerEnd++
     }
     
 
