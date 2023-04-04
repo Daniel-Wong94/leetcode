925a88c450b901204a1fd05f0d28a34354cf97fd
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-04 16:55:54 -0400

    Time: 102 ms (62.28%), Space: 48.1 MB (61.08%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.js
+++ 1169-invalid-transactions/1169-invalid-transactions.js
@@ -5,26 +5,29 @@
 const invalidTransactions = transactions => {
     const history = {}
     
+    // keep track of transactions of each user
     for (const transaction of transactions) {
         const [name, time, amount, city] = transaction.split(",")
         
-        if (name in history) history[name].push({time, city})
-        else history[name] = [{time, city}]
+        if (!history.hasOwnProperty(name)) history[name] = []
+        history[name].push({time, city})
     }
     
+    // check each transaction for validity
     return transactions.filter(t => isInvalid(t, history))
 }
 
 const isInvalid = (transaction, history) => {
     const [tName, tTime, tAmount, tCity] = transaction.split(",")
-        
-    if (tAmount > 1000) return true;
     
-    for (const {city, time} of history[tName]) {
-        if (Math.abs(time - tTime) <= 60 && tCity !== city) {
+    if (tAmount > 1000) return true
+    
+    // iterate through the user's transaction history
+    for (const {time, city} of history[tName]) {
+        if (Math.abs(time - tTime) <= 60 && city !== tCity) {
             return true
         }
     }
     
-    return false;
+    return false
 }
