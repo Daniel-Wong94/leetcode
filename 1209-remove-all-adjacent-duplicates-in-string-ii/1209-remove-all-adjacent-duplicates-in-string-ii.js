52574ca07ac4bbaf1373f168aeff3a71c5fcfca8
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-11 15:27:20 -0400

    Time: 81 ms (79.92%), Space: 50.2 MB (56.15%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -4,20 +4,23 @@
  * @return {string}
  */
 var removeDuplicates = function(s, k) {
-    let stack = []
+    let stack = [] // elements = [char, numOfRepeat]
     
     for (const c of s){
+        // compare current char with last stack's char
         if (stack.length && stack[stack.length - 1][0] === c) {
             stack[stack.length - 1][1]++
         } else {
             stack.push([c, 1])
         }
         
+        // check if last stack's char count === k
         if (stack[stack.length - 1][1] === k) {
             stack.pop()
         }
     }
     
+    // stack should have chars and their count in order; build string
     let result = ""
     
     stack.forEach(([char, num]) => result += char.repeat(num))
