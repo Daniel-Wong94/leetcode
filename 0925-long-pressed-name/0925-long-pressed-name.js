3cbf6dc1a64729bb1c9f9bf1d5a1b049ddd445f4
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-11 13:13:38 -0400

    Time: 61 ms (60.12%), Space: 41.9 MB (88.34%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} name
+ * @param {string} typed
+ * @return {boolean}
+ */
+var isLongPressedName = function(name, typed) {
+    let namePointer = typedPointer = 0
+    
+    while (typedPointer < typed.length) {
+        if (name[namePointer] === typed[typedPointer]) {
+            namePointer++
+            typedPointer++
+        } else {
+            if (name[namePointer - 1] === typed[typedPointer]) {
+                typedPointer++
+            } else {
+                return false
+            }
+        }
+    }
+    
+    return namePointer === name.length
+};
