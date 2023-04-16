f93e358cb9df1991c9c59ed9e0afb3c56d603b9c
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-16 15:29:23 -0400

    Time: 46 ms (97.47%), Space: 41.8 MB (73.90%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -4,13 +4,18 @@
  */
 var rob = function(nums, i = 0, memo = {}) {
     if (i in memo) return memo[i]
-    if (i >= nums.length) return 0
-    let sum = 0 
     
+    // base case: exhausted all elements of the array
+    if (i >= nums.length) return 0
+    
+    let sum = 0 
+    // there's two decisions: take the first element or don't take the first element
     const attempt1 = nums[i] + rob(nums, i + 2, memo)
     const attempt2 = rob(nums, i + 1, memo)
     
+    // bubble up the greater values and cache it in memo
     sum += Math.max(attempt1, attempt2)
     memo[i] = sum
+    
     return memo[i];
 };
