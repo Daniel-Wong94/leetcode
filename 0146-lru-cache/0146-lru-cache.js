92efb6ec5cc69399ac9cc150ef9fc394a8e8f1f0
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-16 18:21:42 -0400

    Time: 667 ms (30.63%), Space: 92.3 MB (93.10%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -3,40 +3,40 @@
  */
 
 // use a Map: access key in O(1) and remembers order of insertion
-var LRUCache = function(capacity) {
-    this.capacity = capacity
-    this.cache = new Map()
-};
-
-/** 
+class LRUCache {
+    constructor(capacity) {
+        this.capacity = capacity
+        this.cache = new Map()
+    }
+ /** 
  * @param {number} key
  * @return {number}
- */
-LRUCache.prototype.get = function(key) {
-    if (!this.cache.has(key)) return -1
-    
-    const val = this.cache.get(key)
-    this.cache.delete(key)
-    this.cache.set(key, val)
-    
-    return this.cache.get(key)
-};
+ */ 
+    get = (key) => {
+        if (!this.cache.has(key)) return -1
 
+        const val = this.cache.get(key)
+        this.cache.delete(key)
+        this.cache.set(key, val)
+
+        return this.cache.get(key)    
+    }
 /** 
  * @param {number} key 
  * @param {number} value
  * @return {void}
  */
-LRUCache.prototype.put = function(key, value) {
-    if (this.cache.has(key)) {
-        this.cache.delete(key)
-    }
-    
-    this.cache.set(key, value)
-    
-    if (this.cache.size > this.capacity) {
-        const recent = this.cache.keys().next()
-        this.cache.delete(recent.value)
+    put = (key, value) => {
+        if (this.cache.has(key)) {
+            this.cache.delete(key)
+        }
+
+        this.cache.set(key, value)
+
+        if (this.cache.size > this.capacity) {
+            const recent = this.cache.keys().next()
+            this.cache.delete(recent.value)
+        }
     }
 };
 
