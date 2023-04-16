c10ae43af82385e12f8ce5a8b05f4e553bcaa1be
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-16 18:24:22 -0400

    Time: 666 ms (31.83%), Space: 93.3 MB (77.09%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -8,24 +8,25 @@ class LRUCache {
         this.capacity = capacity
         this.cache = new Map()
     }
- /** 
- * @param {number} key
- * @return {number}
- */ 
+     /** 
+     * @param {number} key
+     * @return {number}
+     */ 
     get = (key) => {
         if (!this.cache.has(key)) return -1
 
+        // get and save the value, then re-set the key to move the order up
         const val = this.cache.get(key)
         this.cache.delete(key)
         this.cache.set(key, val)
 
         return this.cache.get(key)    
     }
-/** 
- * @param {number} key 
- * @param {number} value
- * @return {void}
- */
+    /** 
+     * @param {number} key 
+     * @param {number} value
+     * @return {void}
+     */
     put = (key, value) => {
         if (this.cache.has(key)) {
             this.cache.delete(key)
