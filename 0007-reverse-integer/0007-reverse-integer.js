792b785e354736e4c3c5d9bc51a4bff3cf098afc
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-18 18:09:44 -0400

    Time: 69 ms (72.41%), Space: 43.5 MB (80.51%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -13,12 +13,14 @@ var reverse = function(x) {
     
     while (x > 0) {
         const lastDigit = x % 10
-        result *= 10
-        result += lastDigit
+        result = lastDigit + (result * 10)
         
         x = Math.floor(x / 10)
     }
     
+    // check for overflow
     if (result > Math.pow(2, 31)) return 0
+    
+    // revert to negative
     return isNegative ? result * -1 : result
 };
