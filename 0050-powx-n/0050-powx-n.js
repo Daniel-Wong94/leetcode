ac7297d288f309a022e60c358f921a195ceb59a5
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-20 17:23:43 -0400

    Time: 51 ms (94.05%), Space: 42.5 MB (25.06%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -10,7 +10,12 @@ var myPow = function(x, n) {
     const pow = Math.abs(n)
     const isEvenPow = pow % 2 === 0
     const isPosN = n > 0
+    
+    // 2^4 = 2 * 2 * 2 * 2 is the same as 4 ^ 2 = 4 * 4
+    // 2^5 = 2 * 2 * 2 * 2 * 2 is the same as (4 ^ 2) * 2 = 4 * 4 * 2
     const result = isEvenPow ? myPow(x * x, pow / 2) : myPow(x * x, (pow - 1) / 2) * x
     
+    
+    // if power is negative, return the inverse of it
     return isPosN ? result : 1 / result
 };
