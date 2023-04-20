b44b0dd59df7405ff3c7012fa2322044cd3fb15b
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-20 13:50:23 -0400

    Time: 67 ms (16.92%), Space: 41.9 MB (51.61%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -5,6 +5,7 @@
 var decodeString = function(s) {
     const stack = []
     
+    // keep pushing to stack until ']' is found, then decode the latest string
     for (const char of s) {
         if (char === "]") {
             const decode = []
@@ -15,6 +16,7 @@ var decodeString = function(s) {
             
             stack.pop() // remove the next "[" from stack
             
+            // build up the multiplier
             let times = 0
             let base = 1;
             
@@ -22,7 +24,8 @@ var decodeString = function(s) {
                 times = times + stack.pop() * base
                 base *= 10
             }
-                        
+            
+            // push the decoded string back into stack
             while (times !== 0) {
                 for (let i = decode.length - 1; i >= 0; i--) {
                     stack.push(decode[i])
@@ -31,6 +34,7 @@ var decodeString = function(s) {
                 times--
             }
         } else {
+        // if neither '[' nor ']', then must be a char
             stack.push(char)
         }
     }
