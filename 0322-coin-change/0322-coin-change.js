fbe43324813059c3e54e2417ca9c09074a648519
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-04-27 12:50:22 -0400

    Time: 262 ms (14.88%), Space: 50.5 MB (17.29%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let numCoins = _coinChange(coins, amount)
+    
+    if (numCoins === Infinity) return -1
+    else return numCoins
+};
+
+
+const _coinChange = (coins, amount, memo = {}) => {
+    if (amount in memo) return memo[amount]
+    if (amount === 0) return 0
+    if (amount < 0) return Infinity
+    
+    let min = Infinity
+    
+    for (const coin of coins) {
+       let numCoins = 1 + _coinChange(coins, amount - coin, memo)
+       min = Math.min(min, numCoins)
+    }
+    
+    memo[amount] = min
+    return memo[amount]
+}
