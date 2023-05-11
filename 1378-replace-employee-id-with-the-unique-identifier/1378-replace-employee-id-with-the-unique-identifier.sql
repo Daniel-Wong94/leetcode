89a91ef233e1907904f84c15f1a9b195185b67a8
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-05-11 19:32:47 -0400

    Time: 2337 ms (40.02%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -2,3 +2,5 @@
 SELECT name, unique_id FROM Employees
 LEFT JOIN EmployeeUNI
 ON Employees.id = EmployeeUNI.id
+# Left join: returns everything from left table (Employees) and joins 
+# the rows on the right table (EmployeeUNI) that matches ON id
