8b34e3767277eb9a4cb2de5c027c73432d0664ca
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-05-15 19:03:11 -0400

    Time: 77 ms (14.36%), Space: 44.6 MB (61.82%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const original = init;
+    let value = init
+    
+    return {
+        increment: () => ++value,
+        decrement: () => --value,
+        reset: () => {
+            value = original
+            return value
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
