8fa61f3a012142dd8422fb27c27d761cb6edbb4a
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-06 15:39:05 -0400

    Time: 82 ms (48.89%), Space: 47.3 MB (8.89%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} colors
+ * @return {boolean}
+ */
+
+// get the longest substring of A and B
+// if there's AAABAAABBB, we know that it's impossible for that first B to be picked
+// with the longest substring, if it's a length of 5, max moves is length - 2
+// the longer substring will win
+var winnerOfGame = function(colors) {
+    if (colors.length < 3) return false;
+    
+    let movesA = movesB = 0
+    let previousColor = colors[0]
+    let streak = 0
+    
+    for (const color of colors) {
+        if (color === previousColor) streak++
+        else {
+            if (previousColor === "A" && streak >= 3) movesA += streak - 2 
+            if (previousColor === "B" && streak >= 3) movesB += streak - 2 
+            previousColor = color
+            streak = 1
+        }
+    }
+    
+    if (previousColor === "A" && streak >= 3) movesA += streak - 2 
+    if (previousColor === "B" && streak >= 3) movesB += streak - 2 
+    
+
+    return movesA > movesB
+};
