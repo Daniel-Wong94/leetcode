39bf8d960a672f3ac46209c727631561761c768e
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-06 10:39:17 -0400

    Time: 54 ms (91.67%), Space: 42.1 MB (93.33%) - LeetHub

--- 0243-shortest-word-distance/0243-shortest-word-distance.js
+++ 0243-shortest-word-distance/0243-shortest-word-distance.js
@@ -5,16 +5,25 @@
  * @return {number}
  */
 var shortestDistance = function(wordsDict, word1, word2) {
-    let min = Infinity
+    let [i1, i2] = [-1, -1]
+    let min = wordsDict.length
+    
     for (let i = 0; i < wordsDict.length; i++) {
-        if (wordsDict[i] === word1) {
-            for (let j = 0; j < wordsDict.length; j++) {
-                if (wordsDict[j] === word2) {
-                    min = Math.min(min, Math.abs(i - j))
-                }
-            }
+        const word = wordsDict[i]
+        
+        if (word === word1) {
+            i1 = i
+        }
+        
+        if (word === word2) {
+            i2 = i
+        }
+        
+        if (i1 > -1 && i2 > -1) {
+            min = Math.min(min, Math.abs(i1 - i2))
         }
     }
+    
     
     return min
 };
