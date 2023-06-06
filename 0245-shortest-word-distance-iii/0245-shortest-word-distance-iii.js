d43b3b52ef6803c90491fd51b045a3ff9b494f10
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-06 10:56:47 -0400

    Time: 122 ms (28.13%), Space: 54.4 MB (65.63%) - LeetHub

--- 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.js
+++ 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.js
@@ -7,6 +7,10 @@
 
 // same logic as shortest distance I, but now we have to handle if word1 === word2
 // if word1 === word2 then we have to check which pointer gets updated
+// situations for word1 === word2:
+// first time finding a word: assign i to i1
+// second time finding a word: assign i to i2
+// nth time finding a word: shift i1 to where i2 was, assign i to i2
 var shortestWordDistance = function(wordsDict, word1, word2) {
     let [i1, i2] = [-1, -1]
     let min = wordsDict.length
