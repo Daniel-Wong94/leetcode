9266900e72ebc934061cfbda663270aaf685d187
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-06 10:41:14 -0400

    Time: 67 ms (26.67%), Space: 42.4 MB (70.00%) - LeetHub

--- 0243-shortest-word-distance/0243-shortest-word-distance.js
+++ 0243-shortest-word-distance/0243-shortest-word-distance.js
@@ -4,6 +4,11 @@
  * @param {string} word2
  * @return {number}
  */
+
+// use two pointers and update their most recent position in one pass
+// situations:
+// - you pass the same word1 or word2 in a row, you know the first one can't be the shortest then, so you can disregard the first position
+// - you find both a word1 and word2, so update the min
 var shortestDistance = function(wordsDict, word1, word2) {
     let [i1, i2] = [-1, -1]
     let min = wordsDict.length
@@ -11,13 +16,8 @@ var shortestDistance = function(wordsDict, word1, word2) {
     for (let i = 0; i < wordsDict.length; i++) {
         const word = wordsDict[i]
         
-        if (word === word1) {
-            i1 = i
-        }
-        
-        if (word === word2) {
-            i2 = i
-        }
+        if (word === word1) i1 = i
+        if (word === word2) i2 = i
         
         if (i1 > -1 && i2 > -1) {
             min = Math.min(min, Math.abs(i1 - i2))
