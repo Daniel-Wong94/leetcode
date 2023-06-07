33c1903979658389cc476ba5145ab6b8c4b0ed97
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-07 17:16:26 -0400

    Time: 93 ms (6.26%), Space: 50.1 MB (9.98%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    const order = inOrder(root)
+    return order[order.length - k]
+};
+
+const inOrder = root => {
+    if (!root) return [];
+    if (!root.left && !root.right) return [root.val]
+    
+    
+    const left = inOrder(root.left)
+    const right = inOrder(root.right)
+    
+    return [...right, root.val, ...left]
+}
