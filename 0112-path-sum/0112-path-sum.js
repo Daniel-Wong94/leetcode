1fc4bd55d9e8fd0dda9453b9ebef590cb96bf6c1
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-08 19:25:52 -0400

    Time: 69 ms (63.27%), Space: 47 MB (7.05%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false;
+    
+    const stack = [ [root, targetSum] ]
+    
+    while (stack.length) {
+        const [ node, target ] = stack.pop()
+        
+        if (!node.left && !node.right && node.val === target) return true; 
+        
+        if (node.left) stack.push([node.left, target - node.val])
+        if (node.right) stack.push([node.right, target - node.val])
+    }
+    
+    return false;
+};
