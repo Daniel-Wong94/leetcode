6ce3201fc16f8aaeed14b143446576c7c9557fbc
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-08 19:17:57 -0400

    Time: 67 ms (43.24%), Space: 43.5 MB (86.49%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
@@ -10,29 +10,57 @@
  * @param {number[]} preorder
  * @return {TreeNode}
  */
-var bstFromPreorder = function(preorder) {
-    if (!preorder.length) return null
-    
-    const inorder = preorder.slice().sort((a, b) => a - b)
-    
-    return build(inorder, preorder)
-};
 
-const build = (inorder, preorder) => {
-    if (!inorder.length) return null
+// naive: sort the preorder so you have both preorder and inorder
+// use the same logic in construct bst with preorder and inorder
+// var bstFromPreorder = function(preorder) {
+//     if (!preorder.length) return null
     
-    const rootVal = preorder[0]
-    const root = new TreeNode(rootVal)
+//     const inorder = preorder.slice().sort((a, b) => a - b)
     
-    const mid = inorder.indexOf(rootVal)
-    const leftInorder = inorder.slice(0, mid)
-    const rightInorder = inorder.slice(mid + 1)
+//     return build(inorder, preorder)
+// };
+
+// const build = (inorder, preorder) => {
+//     if (!inorder.length) return null
     
-    const leftPreorder = preorder.slice(1, leftInorder.length + 1)
-    const rightPreorder = preorder.slice(leftInorder.length + 1)
+//     const rootVal = preorder[0]
+//     const root = new TreeNode(rootVal)
     
-    root.left = build(leftInorder, leftPreorder)
-    root.right = build(rightInorder, rightPreorder)
+//     const mid = inorder.indexOf(rootVal)
+//     const leftInorder = inorder.slice(0, mid)
+//     const rightInorder = inorder.slice(mid + 1)
     
-    return root
+//     const leftPreorder = preorder.slice(1, leftInorder.length + 1)
+//     const rightPreorder = preorder.slice(leftInorder.length + 1)
+    
+//     root.left = build(leftInorder, leftPreorder)
+//     root.right = build(rightInorder, rightPreorder)
+    
+//     return root
+// }
+
+const bstFromPreorder = (preorder) => {
+    // global i
+    let i = 0;
+    
+    const build = (preorder, low = -Infinity, high = Infinity) => {
+        // base case: if we iterate through all the elements, return null
+        if (i >= preorder.length) return null;
+
+        const val = preorder[i]
+        // base case: if the preorder[i] indicates that we've completed the branch return null
+        if (val < low || val > high) return null;
+        
+        i += 1;  
+        
+        const root = new TreeNode(val)
+
+        root.left = build(preorder, low, val)
+        root.right = build(preorder, val, high)
+
+        return root;
+    }
+    
+    return build(preorder)
 }
