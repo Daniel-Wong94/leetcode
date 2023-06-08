e0d21e90c262ff685037b0526e9d2d7d888d1c69
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-08 19:26:58 -0400

    Time: 61 ms (90.35%), Space: 46.4 MB (14.35%) - LeetHub

--- 0112-path-sum/0112-path-sum.js
+++ 0112-path-sum/0112-path-sum.js
@@ -12,19 +12,25 @@
  * @return {boolean}
  */
 
-var hasPathSum = function(root, targetSum) {
-    if (!root) return false;
+// var hasPathSum = function(root, targetSum) {
+//     if (!root) return false;
     
-    const stack = [ [root, targetSum] ]
+//     const stack = [ [root, targetSum] ]
     
-    while (stack.length) {
-        const [ node, target ] = stack.pop()
+//     while (stack.length) {
+//         const [ node, target ] = stack.pop()
         
-        if (!node.left && !node.right && node.val === target) return true; 
+//         if (!node.left && !node.right && node.val === target) return true; 
         
-        if (node.left) stack.push([node.left, target - node.val])
-        if (node.right) stack.push([node.right, target - node.val])
-    }
+//         if (node.left) stack.push([node.left, target - node.val])
+//         if (node.right) stack.push([node.right, target - node.val])
+//     }
     
-    return false;
-};
+//     return false;
+// };
+
+const hasPathSum = (root, target) => {
+    if (!root) return false
+    if (!root.left && !root.right && root.val === target) return true
+    return hasPathSum(root.left, target - root.val) || hasPathSum(root.right, target - root.val)
+}
