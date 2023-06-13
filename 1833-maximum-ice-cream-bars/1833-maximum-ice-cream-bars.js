e3e38d68d872c423a89971cf5450b54f36cd0a20
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-13 15:12:06 -0400

    Time: 81 ms (100.00%), Space: 55.3 MB (83.19%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -20,18 +20,24 @@
 
 // use an array to track the freq of each cost
 const maxIceCream = (costs, coins) => {
+    // array length at max costs, all starting at 0
     const freq = Array(Math.max(...costs) + 1).fill(0)
     
+    // increment the freq array, where i = cost, freq[i] = number of times of that cost
     costs.forEach(cost => freq[cost]++)
     
     let result = 0
     
+    // iterate from 1 since there will never be a cost of 0
     for (let i = 1; i < freq.length; i++) {
+        // skip the empty elements, and break the loop when the coins are insufficient
         if (freq[i] === 0) continue;
         if (coins < i) break;
         
+        // if you can afford more than the freq[i], save the count as freq[i]
         const count = Math.min(freq[i], Math.floor(coins / i))
         
+        // deduct the coins by the cost * count
         coins -= i * count;
         result += count
     }
