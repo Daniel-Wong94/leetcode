ab8e50af5b7cd79fc39ef21b0bf247354f2ef07a
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-13 19:47:33 -0400

    Time: 293 ms (34.51%), Space: 79.4 MB (5.70%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0] || a[1] - b[1])
+    console.log(intervals)
+    let result = 0
+    let prev = intervals[0]
+    
+    for (let i = 1; i < intervals.length; i++) {
+        const curr = intervals[i]
+        
+        if (curr[0] < prev[1]) {
+            result++
+            prev = prev[1] < curr[1] ? prev : curr
+        } else {
+            prev = curr
+        }
+    }
+    
+    return result
+};
