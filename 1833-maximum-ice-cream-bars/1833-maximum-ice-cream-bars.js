9b92099c8324ce20883b61ba3f59b97a64f2c6c6
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-13 15:07:31 -0400

    Time: 113 ms (88.24%), Space: 55.8 MB (73.11%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -3,15 +3,38 @@
  * @param {number} coins
  * @return {number}
  */
-var maxIceCream = function(costs, coins) {
-    costs.sort((a, b) => a - b)
+
+// sort ascending and count how many icecreams can be bought
+// var maxIceCream = function(costs, coins) {
+//     costs.sort((a, b) => a - b)
     
-    let i = 0
+//     let i = 0
     
-    while (i < costs.length && coins >= costs[i]) {
-        coins -= costs[i]
-        i++
+//     while (i < costs.length && coins >= costs[i]) {
+//         coins -= costs[i]
+//         i++
+//     }
+    
+//     return i
+// };
+
+// use an array to track the freq of each cost
+const maxIceCream = (costs, coins) => {
+    const freq = Array(Math.max(...costs) + 1).fill(0)
+    
+    costs.forEach(cost => freq[cost]++)
+    
+    let result = 0
+    
+    for (let i = 1; i < freq.length; i++) {
+        if (freq[i] === 0) continue;
+        if (coins < i) break;
+        
+        const count = Math.min(freq[i], Math.floor(coins / i))
+        
+        coins -= i * count;
+        result += count
     }
     
-    return i
-};
+    return result
+}
