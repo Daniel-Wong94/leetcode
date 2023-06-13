c9e96fc506ce38fa6b54fc4a34e4e7c42399bdeb
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-13 14:40:04 -0400

    Time: 258 ms (34.45%), Space: 55.2 MB (85.71%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -6,14 +6,12 @@
 var maxIceCream = function(costs, coins) {
     costs.sort((a, b) => a - b)
     
-    let result = 0 
     let i = 0
     
     while (i < costs.length && coins >= costs[i]) {
         coins -= costs[i]
-        result++
         i++
     }
     
-    return result
+    return i
 };
