c3f3931add451ad872b0af4e10eff53095f299c5
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-13 20:13:31 -0400

    Time: 59 ms (53.76%), Space: 41.3 MB (97.54%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
@@ -7,17 +7,12 @@
 // if there is no num < 1, return 0
 // else return Math.abs(most negative number + 1)
 var minStartValue = function(nums) {
-    const runningSum = []
     let min = Infinity
+    let total = 0
     
     for (let i = 0; i < nums.length; i++) {
-        if (i === 0) {
-            runningSum[i] = nums[i]
-        } else {
-            runningSum[i] = runningSum[i - 1] + nums[i]
-        }
-        
-        min = Math.min(min, runningSum[i])
+        total += nums[i]
+        min = Math.min(min, total)
     }
     
     return min < 1 ? Math.abs(min) + 1 : 1
