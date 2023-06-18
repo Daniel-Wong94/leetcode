126e38f6b743567522b9877d37e5ffd40f2b68c2
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-18 10:46:09 -0400

    Time: 67 ms (30.46%), Space: 42.3 MB (59.41%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -2,6 +2,12 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+// from Buy and Sell part I
+// instead of finding the greatest difference, we now need to add up all the positive differences
+// edge case, the stock prices always increase:
+// since we can buy and sell each day, we can crawl through the slope
+// buying day 1 and selling day 10 is the same as buy/sell each day
 var maxProfit = function(prices) {
     let maxProfit = 0
     
