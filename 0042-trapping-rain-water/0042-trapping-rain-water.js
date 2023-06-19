cbb71a07ec28118b5b0fbde30249e96cf824f240
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-19 11:13:39 -0400

    Time: 78 ms (30.15%), Space: 46.3 MB (21.15%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,62 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+
+// // at each height, find the max height to the left and right of it
+// // since the min height of the two maxes is the bottleneck, we'll select that value
+// // subtract the current height from the selected value
+// // if the value is positive, add it to our result
+// // this will timeout time: O(n^2) space: O(1)
+// const trap = heights => {
+//     let result = 0
+
+//     for (let i = 1; i < heights.length; i++) {
+//         const maxLeft = Math.max(...heights.slice(0, i))
+//         const maxRight = Math.max(...heights.slice(i + 1))
+//         const bottleNeck = Math.min(maxLeft, maxRight)
+//         const value = bottleNeck - heights[i]
+        
+//         if (value > 0) result += value
+//     }
+    
+//     return result;
+// }
+
+// sacrifice space for time
+// have two arrays that correspond to the maxLeft and maxRight at index, i
+// iterate through the heights and do the same logic
+const trap = heights => {
+    let result = 0
+    const maxLeft = []
+    const maxRight = []
+    
+    let currMaxLeft = 0
+    let currMaxRight = 0
+    
+    for (let i = 0; i < heights.length; i++) {
+        const leftHeight = heights[i]
+        const rightHeight = heights[heights.length - 1 - i]
+        
+        if (leftHeight > currMaxLeft) {
+            currMaxLeft = leftHeight
+        }
+        
+        if (rightHeight > currMaxRight) {
+            currMaxRight = rightHeight
+        }
+        
+        maxLeft[i] = currMaxLeft
+        maxRight[heights.length - 1 - i] = currMaxRight
+    }
+    
+    for (let i = 0; i < heights.length; i++) {
+        const bottleNeck = Math.min(maxLeft[i], maxRight[i])
+        const value = bottleNeck - heights[i]
+        
+        if (value > 0) result += value
+    }
+    
+    return result
+}
