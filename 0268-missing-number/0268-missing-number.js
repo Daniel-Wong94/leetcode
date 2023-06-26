fa9c94518bd64b0178c1a1719ccbe16a32a9fe85
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-06-26 17:46:16 -0400

    Time: 73 ms (48.90%), Space: 48.3 MB (6.94%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    const unique = new Set(nums)
+    
+    for (let i = 0; i <= nums.length; i++) {
+        if (!unique.has(i)) return i
+    }
+};
