1e33b61abd16ece58b202b8724f2bea58a5894de
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-07-17 12:33:18 -0400

    Time: 107 ms (62.83%), Space: 51.9 MB (12.85%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -9,9 +9,13 @@ var merge = function(intervals) {
     
     for (const [currStart, currEnd] of intervals) {
         const mostRecent = result[result.length - 1]
+        
+        // if we're on the first iteration, just push
+        // if the most recent end is before the current start, then it's not overlapped
         if (result.length === 0 || mostRecent[1] < currStart) {
             result.push([currStart, currEnd])
         } else {
+            // if we've sorted our intervals by start time, then we just have to update endtime
             mostRecent[1] = Math.max(mostRecent[1], currEnd)
         }
     }
