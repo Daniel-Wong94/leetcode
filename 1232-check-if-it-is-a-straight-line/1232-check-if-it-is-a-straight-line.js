c8521d0c46750246b41aea88403f536536d8393e
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-07-19 16:13:25 -0400

    Time: 68 ms (30.85%), Space: 43.7 MB (74.82%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
@@ -2,6 +2,12 @@
  * @param {number[][]} coordinates
  * @return {boolean}
  */
+
+// get the slope between the first two coordinates
+// compare this slope with every slope between coordinate[0] and coordinate[i] 
+// if deltaY1 / deltaX1 is the slope of coordinate[0] and [1]
+// then deltaY2 / deltaX2 of coordinate[0] and [i] should equal that
+// deltaY1 * deltaX2 === deltaY2 * deltaX1
 var checkStraightLine = function(coordinates) {
     const deltaY = coordinates[1][1] - coordinates[0][1]
     const deltaX = coordinates[1][0] - coordinates[0][0]
