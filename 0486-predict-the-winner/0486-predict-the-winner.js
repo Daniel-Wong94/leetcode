2069f05a92f6dd29bc52419f2e2153cbd42c89ce
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-07-26 13:55:55 -0400

    Time: 364 ms (9.52%), Space: 42.4 MB (78.57%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+// can't greedy method because it can lead to player2 getting a bigger score
+// dp: compare sum of the left vs right decisions, choosing the max score after decision
+var PredictTheWinner = function(nums) {
+    
+    const getWinner = (left, right) => {
+        if (left === right) return nums[left]
+
+        const takeLeft = nums[left] - getWinner(left + 1, right)
+        const takeRight = nums[right] - getWinner(left, right - 1)
+
+        return Math.max(takeLeft, takeRight)
+    }
+    
+    return getWinner(0, nums.length - 1) >= 0
+};
