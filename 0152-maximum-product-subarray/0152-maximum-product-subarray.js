a026d6e3ff120a8d6a8cca2560f88e7e88c7f772
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-07-31 12:59:37 -0400

    Time: 387 ms (5.01%), Space: 42.8 MB (42.99%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    if (nums.length === 0) return 0
+    if (nums.length === 1) return nums[0]
+    let max = nums[0]
+    
+    for (let i = 0; i < nums.length; i++) {
+        let curr = 1
+        for (let j = i; j < nums.length; j++) {
+            curr *= nums[j]
+            max = Math.max(max, curr)
+        }
+    }
+    
+    return max
+};
