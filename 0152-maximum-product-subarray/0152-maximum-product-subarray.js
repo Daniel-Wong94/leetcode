8efc3928bbe4616e6ecffc3ced9284c04f9fbb1a
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-08-01 10:55:17 -0400

    Time: 45 ms (99.41%), Space: 42.1 MB (84.97%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -2,17 +2,37 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxProduct = function(nums) {
-    if (nums.length === 0) return 0
-    let max = nums[0]
+// var maxProduct = function(nums) {
+//     if (nums.length === 0) return 0
+//     let max = nums[0]
     
-    for (let i = 0; i < nums.length; i++) {
-        let curr = 1
-        for (let j = i; j < nums.length; j++) {
-            curr *= nums[j]
-            max = Math.max(max, curr)
-        }
+//     for (let i = 0; i < nums.length; i++) {
+//         let curr = 1
+//         for (let j = i; j < nums.length; j++) {
+//             curr *= nums[j]
+//             max = Math.max(max, curr)
+//         }
+//     }
+    
+//     return max
+// };
+
+const maxProduct = nums => {
+    if (nums.length === 0) return 0
+    
+    let currMax = nums[0]
+    let currMin = nums[0]
+    let result = currMax
+    
+    for (let i = 1; i < nums.length; i++) {
+        const curr = nums[i]
+        const tempMax = Math.max(curr, currMax * curr, currMin * curr)
+        currMin = Math.min(curr, currMax * curr, currMin * curr)
+        
+        currMax = tempMax
+        
+        result = Math.max(currMax, result)
     }
     
-    return max
-};
+    return result
+}
