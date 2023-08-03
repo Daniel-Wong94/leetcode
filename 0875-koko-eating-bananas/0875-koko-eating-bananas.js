faf29c5d7f0e4694681cdadb20bcc1acbb059229
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-08-03 17:39:46 -0400

    Time: 60 ms (97.81%), Space: 45.4 MB (51.82%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,61 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+
+// start off with speed of 1 and keep incrementing the speed
+// until the total time to eat all piles is <= h
+// TLE: O(nm)
+// var minEatingSpeed = function(piles, h) {
+//     let speed = 1
+    
+//     while (true) {
+//         let time = 0
+        
+//         for (const pile of piles) {
+//             time += Math.ceil(pile / speed)
+//         }
+        
+//         if (time <= h) {
+//             return speed
+//         } else {
+//             speed++
+//         }
+//     }
+// };
+
+// instead of trying every speed from 1
+// we can BS with 1 as our lower bound (nothing can be lower than 1)
+// and max(piles) as our upper bound (max will always work, so anything above that will also work)
+// keep BS with those bounds until you find the min speed
+const minEatingSpeed = (piles, h) => {
+    let left = 0
+    let right = Math.max(...piles) // O(n)
+    
+    while (left < right) {
+        const speed = Math.floor((left + right) / 2)
+        
+        const time = eat(piles, h, speed)
+        
+        if (time <= h) {
+            right = speed
+        } else {
+            left = speed + 1
+        }
+    }
+    
+    return left
+}
+
+const eat = (piles, h, speed) => {
+    let time = 0
+    
+    for (const pile of piles) {
+        time += Math.ceil(pile / speed)
+    }
+    
+    return time
+}
+
+
