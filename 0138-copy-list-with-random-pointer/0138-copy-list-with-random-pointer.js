bc7d65c0449615bb36796a0ff9ef54cdbf000f97
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-08-03 12:15:04 -0400

    Time: 57 ms (73.84%), Space: 43.3 MB (94.10%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.js
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.js
@@ -15,15 +15,21 @@
 // by treating this like a graph (there's two pointers: next and random)
 // we see that this a bidirectional cyclic graph
 // therefore, we need to track our visited nodes to prevent cycles
+// map the original node to the cloned node, so if we visit it again, we can just map get
 var copyRandomList = function(head, visited = new Map()) {
+    // base case: node is null
     if (!head) return null
     
+    // check if we already mapped this original node, if so, return the cloned node
     if (visited.has(head)) return visited.get(head);
     
+    // create our clone node
     const current = new Node(head.val, null, null)
     
+    // map the cloned node to the original
     visited.set(head, current)    
     
+    // recursive steps: DFS the .next and .random pointers
     current.next = copyRandomList(head.next, visited)
     current.random = copyRandomList(head.random, visited)   
     
