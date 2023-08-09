5b7742dd44e78a6012d44174fe1e528a65a712d0
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-08-09 15:29:34 -0400

    Time: 82 ms (82.98%), Space: 45.9 MB (46.50%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -3,6 +3,11 @@
  * @param {number} k
  * @return {number}
  */
+
+// find the num of vowels in the window : s[0] to s[k - 1]
+// as you slide the window, check if you should subtract from the streak for s[k - i]
+// and check if you should add to the streak for s[i]
+// keep updating the maxStreak
 var maxVowels = function(s, k) {
     const VOWELS = "aeiou"
     
