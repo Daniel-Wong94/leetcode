aa1d1b41c328e90bb746956643487cc3f4913b12
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-08-09 18:11:29 -0400

    Time: 57 ms (94.84%), Space: 44.9 MB (33.15%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -2,6 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// get the total of the right side of the index
+// iterate and move the pivot point to the right
+// each iteration: add the nums[i] to leftSum first
+// then check if left === right
+// lastly subtract nums[i] from rightSum
 var pivotIndex = function(nums) {
     let rightSum = 0
     
@@ -10,8 +16,10 @@ var pivotIndex = function(nums) {
     let leftSum = 0
     
     for (let i = 0; i < nums.length; i++) {
-        leftSum += nums[i]        
-        if (leftSum === rightSum) return i        
+        leftSum += nums[i]
+        
+        if (leftSum === rightSum) return i
+        
         rightSum -= nums[i]
     }
 
