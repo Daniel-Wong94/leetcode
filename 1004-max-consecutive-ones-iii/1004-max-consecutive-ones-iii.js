87675261ac0ece1d7993608002e959b09c885975
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-08-09 16:34:00 -0400

    Time: 65 ms (80.54%), Space: 47.1 MB (50.42%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+// create window
+// keep track up the number of 0 in window
+// keep the streak as long as the num of 0 is <= k
+// move the left window to maintain 0  count <= k
+var longestOnes = function(nums, k) {
+    let left = 0
+    let right = 0
+    let max = 0
+    let windowZero = 0
+    
+    while (right < nums.length) {
+        if (nums[right] === 0) {
+            k--
+        }
+
+        
+        while (k < 0) {
+            if (nums[left] === 0) {
+                k++
+            }
+            
+            left++
+        }
+        max = Math.max(max, right - left + 1)
+        
+        right++        
+    }
+    
+    return max
+};
