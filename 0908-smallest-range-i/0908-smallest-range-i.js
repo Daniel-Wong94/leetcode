0bfc614cda63c033e8873f5e834d5aad5f9a36fc
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-09-05 10:22:56 -0400

    Time: 57 ms (59.13%), Space: 43.9 MB (69.13%) - LeetHub

---
+++ 0908-smallest-range-i/0908-smallest-range-i.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRangeI = function(nums, k) {
+    if (nums.length === 1) return 0
+    
+    const max = Math.max(...nums) - k
+    const min = Math.min(...nums) + k
+    
+    return max - min < 0 ? 0 : max - min
+};
