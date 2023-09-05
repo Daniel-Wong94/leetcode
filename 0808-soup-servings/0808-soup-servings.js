c747d90be2d174a4610b76bbfecfde644b99ccbf
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-09-05 14:12:56 -0400

    Time: 69 ms (20.24%), Space: 44.8 MB (42.80%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.js
+++ 0808-soup-servings/0808-soup-servings.js
@@ -4,9 +4,11 @@
  */
 
 var soupServings = function(n) {
+    // reduce the total mL to serving sizes (25ml per serving)
     const servings = Math.ceil(n / 25)
     const memo = {}
     
+    // threshold in which probability reaches near 1 and passes the margin of error
     if (n > 4451) return 1
 
     const dpHelper = (i, j) => {
@@ -14,10 +16,11 @@ var soupServings = function(n) {
         if (key in memo) return memo[key]
         
         // base cases
-        if (i <= 0 && j <= 0) return 0.5
-        if (i <= 0) return 1
-        if (j <= 0) return 0
+        if (i <= 0 && j <= 0) return 0.5        // if both reaches 0, then there's a 50% chance of soup A reaching 0 first
+        if (i <= 0) return 1                    // soup A reaching 0 first is a 100% chance
+        if (j <= 0) return 0                    // soup B reaching 0 first is a 0% chance of A reaching 0 first
         
+        // memoize 25% chance of total probabilty of the four choices you can make
         memo[key] = 0.25 * (dpHelper(i - 4, j) + dpHelper(i - 3, j - 1) + dpHelper(i - 2, j - 2) + dpHelper(i - 1, j - 3))
         return memo[key]
     }
