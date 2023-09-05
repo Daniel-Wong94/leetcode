1c928e0f780d03c05a79687c382695127a0ead2c
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-09-05 10:26:11 -0400

    Time: 54 ms (71.30%), Space: 43.8 MB (77.83%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.js
+++ 0908-smallest-range-i/0908-smallest-range-i.js
@@ -4,10 +4,13 @@
  * @return {number}
  */
 var smallestRangeI = function(nums, k) {
+    // if nums only has 1 element, the max and min will always be that same element
     if (nums.length === 1) return 0
     
+    // minimize the max and maximize the min with k
     const max = Math.max(...nums) - k
     const min = Math.min(...nums) + k
     
+    // if the diff is within 2*k, then return 0
     return Math.max(0, max - min)
 };
