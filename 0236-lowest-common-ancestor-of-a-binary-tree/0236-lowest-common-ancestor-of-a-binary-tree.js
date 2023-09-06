c050585c6fc6068f019017c72d4bc21f29e74345
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-09-06 11:54:15 -0400

    Time: 71 ms (59.85%), Space: 52 MB (39.13%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -15,16 +15,9 @@ var lowestCommonAncestor = function(root, p, q) {
     // base case: reach null or reach p or q
     if (!root || root === p || root === q) return root
     
+    // recursively step left and right
     const left = lowestCommonAncestor(root.left, p, q)
     const right = lowestCommonAncestor(root.right, p , q)
     
-    if (left && right) return root
-    else return left || right
-};
-
-// function lowestCommonAncestor(root, p, q) {
-//   if (!root || root === p || root === q) return root;
-//   var resL = lowestCommonAncestor(root.left, p, q);
-//   var resR = lowestCommonAncestor(root.right, p, q);
-//   return (resL && resR) ? root : (resL || resR);
-// }
+    return left && right ? root : left || right
+}
