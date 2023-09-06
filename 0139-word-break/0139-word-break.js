501ce27268703f864a3ce4730eb8c9d4248a2822
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-09-06 12:49:08 -0400

    Time: 65 ms (32.29%), Space: 44.4 MB (20.07%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -3,12 +3,13 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function(s, wordDict, start = 0, memo={}) {
+var wordBreak = function(s, wordDict, start=0, memo={}) {
+    // check cache
     if (start in memo) return memo[start]
-    // base case
+    
+    // base case: starting pointer reaches end of s
     if (start === s.length) return true
     
-    // loop through wordDict, recurse when we find that it startsWith
     for (let end = start + 1; end <= s.length; end++) {
         if (wordDict.includes(s.slice(start, end)) && wordBreak(s, wordDict, end, memo)) {
             memo[start] = true
