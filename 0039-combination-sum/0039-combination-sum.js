7c6e2bb7ac0e7f447960b52e8413e85c95848fd6
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-09-11 14:09:29 -0400

    Time: 70 ms (57.02%), Space: 44.8 MB (86.89%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -3,29 +3,33 @@
  * @param {number} target
  * @return {number[][]}
  */
+
+// either take the current candidate or the next
 var combinationSum = function(candidates, target) {
     const result = []
     
-    const dp = (i, total, current = []) => {
-        // base case: if target is 0 then add combination to result
+    const dp = (i, total, current=[]) => {
+        // base case: total === target
+        // push a copy of the current array, to avoid reference issues
         if (total === target) {
             result.push(current.slice())
-            return
+            return;
         }
         
+        // base case: if total exceeds target or run out of candidates, there is no need to recursive
         if (i >= candidates.length || total > target) {
-            return
+            return;
         }
         
-        // recursive step: take current candidates[i]
+        // recursive: take candidates[i] FIRST
         current.push(candidates[i])
         dp(i, total + candidates[i], current)
         
-        // recursive step: take next candidate
+        // recursive: skip candidates[i] and take the next candidate
         current.pop()
         dp(i + 1, total, current)
     }
     
     dp(0, 0)
-    return result
+    return result;
 };
