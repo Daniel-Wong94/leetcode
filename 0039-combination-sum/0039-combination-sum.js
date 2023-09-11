6beadb71a22fd333ef6967691446d88d2787da71
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-09-11 14:03:59 -0400

    Time: 70 ms (57.02%), Space: 44.4 MB (97.03%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    candidates.sort((a, b) => a - b)
+    const result = []
+    
+    const dp = (i, total, current = []) => {
+        // base case: if target is 0 then add combination to result
+        if (total === target) {
+            result.push(current.slice())
+            return
+        }
+        
+        if (i >= candidates.length || total > target) {
+            return
+        }
+        
+        // recursive step: take current candidates[i]
+        current.push(candidates[i])
+        dp(i, total + candidates[i], current)
+        
+        // recursive step: take next candidate
+        current.pop()
+        dp(i + 1, total, current)
+    }
+    
+    dp(0, 0)
+    return result
+};
