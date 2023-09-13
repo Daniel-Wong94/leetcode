de9cfab05eec4244c9373d736e00366c61ee52fb
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-09-13 11:30:26 -0400

    Time: 58 ms (32.29%), Space: 42.9 MB (18.47%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,64 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// two binary searches to find lower and upper bound
+var searchRange = function(nums, target) {
+    const lower = lowerBound(nums, target)
+    if (lower === -1) return [-1, -1]
+    const upper = upperBound(nums, target, lower)
+    
+    return [lower, upper]
+};
+
+const lowerBound = (nums, target) => {
+    let left = 0
+    let right = nums.length - 1
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        
+        if (nums[left] === target) {
+            return left
+        } else if (nums[mid] === target) {
+            if (nums[mid - 1] !== target) {
+                return mid
+            } else {
+                right = mid - 1
+            }
+        } else if (nums[mid] > target) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    
+    return -1
+}
+
+const upperBound = (nums, target, lower) => {
+    let left = lower
+    let right = nums.length - 1
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        
+        if (nums[right] === target) {
+            return right
+        } else if (nums[mid] === target) {
+            if (nums[mid + 1] !== target) {
+                return mid
+            } else {
+                left = mid + 1
+            }
+        } else if (nums[mid] > target) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    
+    return -1    
+}
