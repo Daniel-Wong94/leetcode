98b9c99799585f295581f59f465ff3bfc0483631
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-09-26 14:41:10 -0400

    Time: 1134 ms (80.78%), Space: 26.9 MB (70.14%) - LeetHub

---
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -0,0 +1,33 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        # initilize a 2D matrix with an extra row[0] of 0's and start every row with 0's
+        # in order to handle edge cases
+        ROWS, COLS = len(matrix), len(matrix[0])
+        self.sumMat = [ [0] * (COLS + 1) for r in range(ROWS + 1)]
+        
+        # the prefix sum will be the sum of sumMat[0][0] to sumMat[r][c]
+        for r in range(ROWS):
+            prefix_sum = 0
+            for c in range(COLS):
+                # update prefix_sum
+                prefix_sum += matrix[r][c]
+                
+                # add prefix sum right above
+                prefix_sum_above = self.sumMat[r][c + 1]
+                self.sumMat[r + 1][c + 1] = prefix_sum + prefix_sum_above
+                
+    def sumRegion(self, r1: int, c1: int, r2: int, c2: int) -> int:
+        # offset to our new 2D matrix, sumMat
+        r1, c1, r2, c2 = r1 + 1, c1 + 1, r2 + 1, c2 + 1
+
+        bottom_right = self.sumMat[r2][c2]
+        top_row = self.sumMat[r1 - 1][c2]
+        left_column = self.sumMat[r2][c1 - 1]
+        top_left = self.sumMat[r1 - 1][c1 - 1]
+        
+        return bottom_right - top_row - left_column + top_left
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
