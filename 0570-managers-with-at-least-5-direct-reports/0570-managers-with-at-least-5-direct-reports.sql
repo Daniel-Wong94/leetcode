a5a71e5dda672401bb2c3e3ba36b4d5eefd56622
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-11-06 13:22:29 -0500

    Time: 653 ms (28.10%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,15 +1,9 @@
 # Write your MySQL query statement below
-SELECT
-    Name
-FROM
-    Employee AS t1 
-JOIN
-    (SELECT 
-        ManagerId
-    FROM 
-        Employee
-    GROUP BY ManagerId
-    HAVING COUNT(ManagerId) >= 5) AS t2
-ON 
-    t1.Id = t2.ManagerId
-;
+SELECT name
+FROM Employee AS t1
+JOIN 
+    (SELECT managerId
+     FROM Employee
+     GROUP BY managerId
+     HAVING COUNT(managerId) >= 5) AS t2
+ON t1.id = t2.managerId
