89565581170a2dca0b1be1f30d85127b294bebdc
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-11-06 10:18:04 -0500

    Time: 2323 ms (50.25%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
-SELECT name, unique_id FROM Employees
+SELECT EmployeeUNI.unique_id, Employees.name FROM Employees
 LEFT JOIN EmployeeUNI
-ON Employees.id = EmployeeUNI.id
-# Left join: returns everything from left table (Employees) and joins 
-# the rows on the right table (EmployeeUNI) that matches ON id
+ON Employees.id = EmployeeUNI.id;
