2c14bbf403ba0f1b7906bf921c612006376920eb
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-11-08 16:10:45 -0500

    Time: 610 ms (75.29%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
+# Scenarios
+# 1. id is ODD and not the last seat - swap the id by incrementing
+# 2. id is ODD and IS the last seat - do not swap
+# 3. id is EVEN - swap the id by decrementing
+# Use subquery to gain access to the max number of seats
 SELECT
     (CASE
         WHEN MOD(id, 2) != 0 AND id != max_seat THEN id + 1
