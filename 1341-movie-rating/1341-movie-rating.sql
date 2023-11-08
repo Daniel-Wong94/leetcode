0143961b6d9bc191f629395e01aed74f522638a5
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-11-08 16:35:34 -0500

    Time: 2221 ms (79.22%), Space: 0B (100.00%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,36 @@
+# Write your MySQL query statement below
+# 1. Get the name of user who has rated the most number of movies
+#   - If tie number, return the lexicographically smaller name
+# 2. Get the movie name with highest avg rating in Feb 2020
+#   - If tie, return the lexicographically smaller name
+# Use a union for two separate queries
+
+(SELECT
+   u.name AS results
+FROM
+    MovieRating r
+LEFT JOIN
+    Users u
+ON
+     r.user_id = u.user_id
+GROUP BY
+    r.user_id
+ORDER BY
+    COUNT(r.movie_id) DESC, u.name
+LIMIT 1)
+UNION ALL
+(SELECT
+    m.title AS results
+FROM
+    MovieRating r
+LEFT JOIN
+    Movies m
+ON
+    r.movie_id = m.movie_id
+WHERE
+    LEFT(r.created_at, 7) = "2020-02"
+GROUP BY
+    r.movie_id
+ORDER BY
+    AVG(r.rating) DESC, m.title
+LIMIT 1)
