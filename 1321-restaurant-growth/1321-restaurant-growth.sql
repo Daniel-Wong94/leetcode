942b8b5f69090046d90d36990dfbe90d22dba822
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2023-11-08 17:32:48 -0500

    Time: 555 ms (99.77%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,18 +1,18 @@
 # Write your MySQL query statement below
 # Get moving average of amount in 7 day window (current day + 6)
 #   - round it to two decimal places (ROUND(,2))
-# Use a sliding window, when id is 7, get avg
-# when id > 7, subtract id - 7 and add id
+# For each (GROUP BY) visited_on, get the sum of those 7 days
+# and also the average, excluding dates that do not provide sum(7 days)
 
 SELECT
-    a.visited_on AS visited_on,
+    a.visited_on,
     SUM(b.day_sum) AS amount,
     ROUND(AVG(b.day_sum), 2) AS average_amount
 FROM
-    (SELECT visited_on,SUM(amount) AS day_sum
+    (SELECT visited_on, SUM(amount) AS day_sum  # returns total amount for each visit
     FROM Customer
     GROUP BY visited_on) AS a,
-    (SELECT visited_on, SUM(amount) AS day_sum
+    (SELECT visited_on, SUM(amount) AS day_sum  # returns total amount for each visit
     FROM Customer
     GROUP BY visited_on) AS b
 WHERE
