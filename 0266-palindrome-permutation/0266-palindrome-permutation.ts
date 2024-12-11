c7b059e97b4fc061287cc6849656860767438f56
Daniel Wong <90014250+Daniel-Wong94@users.noreply.github.com>
2024-12-10 20:54:41 -0500

    Time: 4 ms (6.25%), Space: 51.8 MB (5.41%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.ts
+++ 0266-palindrome-permutation/0266-palindrome-permutation.ts
@@ -1,22 +1,14 @@
 function canPermutePalindrome(s: string): boolean {
-    if (s.length === 1) return true
+    let evens = 0
     
     const counter = {}
     
     for (const char of s) {
         counter[char] = counter[char] + 1 || 1
-    }
-    
-    let hasMiddleChar = false
-
-    for (const char in counter) {
-        if (counter[char] % 2 !== 0 && !hasMiddleChar) {
-            hasMiddleChar = true
-            continue
-        }
         
-        if (counter[char] % 2 !== 0 && hasMiddleChar) return false
+        if (counter[char] % 2 === 0) evens++
+        if (counter[char] % 2 !== 0) evens--
     }
-    
-    return true
+    console.log(counter, evens)
+    return -1 <= evens && evens <= 1
 };
